a1a2e08a7216bdd5fb8a3754d07b2c2a0774bbc8
Graudeep <grisha.perevalov@gmail.com>
2021-02-10 13:41:36 +0500

    Добавил страницу с кемпингами.

--- src/js/components/Campings/Campings.js
+++ src/js/components/Campings/Campings.js
@@ -1,14 +1,45 @@
-import React from "react";
-import Footer from "../Footer";
-import Header from "../Header/Header";
+import React, {useEffect, useState} from "react";
+import {connect} from "react-redux";
+import ClipLoader from "react-spinners/ClipLoader";
+import PropTypes from "prop-types";
+import ReactPaginate from 'react-paginate';
+import {Redirect, useLocation} from "react-router-dom";
+import Header from "../Header";
 import "./Campings.scss";
+import Footer from "../Footer";
+import MobileDownbar from "../MobileDownbar/MobileDownBar";
 
-export default () => {
-  return(
+export default function Campings(props) {
+
+  return (
     <>
-      <Header isScrolled = {true}/>
-      <main className = "main__header_scrolled main"> 
-        kek
+      <Header isScrolled = {true}></Header>
+      <main class = "container campings">
+        <section className = "campings__wrapper">
+          <h1 className = "campings__header">Кемпинги</h1>
+          <div className = "campings__filters">
+            <button className = "campings__filter">Специальные</button>
+            <button className = "campings__filter">Места</button>
+            <button className = "campings__filter">Цена</button>
+            <button className = "campings__filter">Дополнительно</button>
+            <button className = "campings__filter">Фильтры (6)</button>
+          </div>
+          <h2 className = "campings__subheader">Все кемпинги (всего: plugged)</h2>
+          <div className = "campings__list">
+            
+          </div>
+          <ReactPaginate 
+            previousLabel = "Пред."
+            nextLabel = "След."
+            pageCount = {5} pageRangeDisplayed = {2} marginPagesDisplayed = {1}
+            containerClassName = "paginate__wrapper"
+            previousClassName = "paginate__link"
+            nextClassName = "paginate__link"
+            disabledClassName = "paginate__link_disabled"
+            activeClassName = "paginate__link_active"
+            />
+        </section>
+        <MobileDownbar />
       </main>
       <Footer />
     </>
