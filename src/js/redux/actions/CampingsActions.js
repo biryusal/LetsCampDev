8cd66744c6bff5bbbce821ae1bfb7250a4ec78ac
Graudeep <grisha.perevalov@gmail.com>
2021-02-12 01:29:13 +0500

    Добавил события взятия данных с БД.

--- src/js/redux/actions/CampingsActions.js
+++ src/js/redux/actions/CampingsActions.js
@@ -1,9 +1,33 @@
-import { GET_CAMPINGS_REQUEST } from "../types/types"
+import { database } from "../../firebase";
+import { getCampingsByPageId } from "../../functions";
+import { GET_CAMPINGS_FAILURE, GET_CAMPINGS_REQUEST, GET_CAMPINGS_SUCCESS } from "../types/types"
 
-export function getCampings() {
-  return function(dispatch) {
+export function getCampings(pageID) {
+  return async function(dispatch) {
     dispatch({
       type: GET_CAMPINGS_REQUEST
     })
+    
+    let campingsList = null,
+        errorMessage = null;
+    
+    try {
+      campingsList = await getCampingsByPageId(pageID);
+    }
+
+    catch(error) {
+      errorMessage = error.message;
+      dispatch({
+        type: GET_CAMPINGS_FAILURE,
+        payload: error.message
+      })
+    }
+
+    if (errorMessage === null) {
+      dispatch({
+        type: GET_CAMPINGS_SUCCESS,
+        payload: campingsList
+      })
+    }
   }
 }
