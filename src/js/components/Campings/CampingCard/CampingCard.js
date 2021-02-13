16fa9c0185dabd27cd870931c8338e588f9eb142
Graudeep <grisha.perevalov@gmail.com>
2021-02-13 19:14:18 +0500

    Поменял ссылку в карточке кемпинга.

--- src/js/components/Campings/CampingCard/CampingCard.js
+++ src/js/components/Campings/CampingCard/CampingCard.js
@@ -5,10 +5,11 @@ import Star from "../../../../img/star.svg";
 import "./CampingCard.scss";
 
 export default (props) => {
-  let {name, rate, pricePerDay, number} = props;
+  let {name, rate, pricePerDay, number} = props,
+      id = number - 1;
   return (
     <div className = "campingCard__item">
-      <NavLink exact to = {"/campings/id/"+ number}>
+      <NavLink exact to = {"/campings/id/"+ id}>
         <div className = "campingCard__imgWrapper">
           <img className = "campingCard__img" src = {CampingPlugImage}></img>
         </div>
@@ -17,13 +18,13 @@ export default (props) => {
         <div className = "campingCard__star">
           <Star />
           <span className = "campingCard__starValue">{rate ? rate : 0}</span>
-          <NavLink exact to = {"/campings/id/"+ number}>
+          <NavLink exact to = {"/campings/id/"+ id}>
             <span className = "campingCard__reviews">25 отзывов</span>
           </NavLink>
         </div>
       </div>
       <div className = "campingCard__info">
-        <NavLink exact to = {"/campings/id/"+ number}>
+        <NavLink exact to = {"/campings/id/"+ id}>
           <h3 className = "campingCard__name">{name}</h3>
         </NavLink>
         <span className = "campingCard__price">{pricePerDay} рублей/ночь</span>
