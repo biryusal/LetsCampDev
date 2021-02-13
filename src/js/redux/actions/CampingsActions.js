462d5395443acbf870621b3f973085e85128b1fd
Graudeep <grisha.perevalov@gmail.com>
2021-02-14 01:11:45 +0500

    Добавил action, который получает текущее количество кемпингов с БД.

--- src/js/redux/actions/CampingsActions.js
+++ src/js/redux/actions/CampingsActions.js
@@ -1,5 +1,5 @@
-import { asyncGetCampingById, getCampingsByPageIdAndSize } from "../../functions";
-import { GET_CAMPINGS_BY_PAGEID_FAILURE, GET_CAMPINGS_BY_PAGEID_REQUEST, GET_CAMPINGS_BY_PAGEID_SUCCESS, GET_CAMPING_BY_ID_FAILURE, GET_CAMPING_BY_ID_REQUEST, GET_CAMPING_BY_ID_SUCCESS } from "../types/types"
+import { asyncGetAmountOfCampings, asyncGetCampingById, getCampingsByPageIdAndSize } from "../../functions";
+import { GET_AMOUNT_OF_CAMPINGS_FAILURE, GET_AMOUNT_OF_CAMPINGS_REQUEST, GET_AMOUNT_OF_CAMPINGS_SUCCESS, GET_CAMPINGS_BY_PAGEID_FAILURE, GET_CAMPINGS_BY_PAGEID_REQUEST, GET_CAMPINGS_BY_PAGEID_SUCCESS, GET_CAMPING_BY_ID_FAILURE, GET_CAMPING_BY_ID_REQUEST, GET_CAMPING_BY_ID_SUCCESS } from "../types/types"
 
 export function getCampingsByPageId(pageID) {
   return async function(dispatch) {
@@ -7,7 +7,7 @@ export function getCampingsByPageId(pageID) {
       type: GET_CAMPINGS_BY_PAGEID_REQUEST
     })
     
-    let campingsList = null,
+    let campingsList = [],
         errorMessage = null;
     
     try {
@@ -15,7 +15,7 @@ export function getCampingsByPageId(pageID) {
     }
 
     catch(error) {
-      errorMessage = error.message;
+      errorMessage = error;
     }
 
     if (errorMessage === null) {
@@ -61,6 +61,39 @@ export function getCampingById(id) {
     else {
       dispatch({
         type: GET_CAMPING_BY_ID_FAILURE,
+        payload: errorMessage
+      })
+    }
+  }
+}
+
+export function getAmountOfCampings() {
+  return async function(dispatch) {
+    let result,
+        errorMessage = null;
+
+    dispatch({
+      type: GET_AMOUNT_OF_CAMPINGS_REQUEST
+    })
+
+    try {
+      result = await asyncGetAmountOfCampings();
+    }
+
+    catch(e) {
+      errorMessage = e.message;
+    }
+
+    if (errorMessage === null) {
+      dispatch({
+        type: GET_AMOUNT_OF_CAMPINGS_SUCCESS,
+        payload: result
+      })
+    }
+
+    else {
+      dispatch({
+        type: GET_AMOUNT_OF_CAMPINGS_FAILURE,
         payload: errorMessage
       })
     }
