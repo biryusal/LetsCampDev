96453b0648d4427d32ea95956b11ab4bcbce0466
Graudeep <grisha.perevalov@gmail.com>
2021-02-13 19:45:49 +0500

    Поменял маршрут у роутера.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import {
 } from "react-router-dom";
 import { connect } from "react-redux";
 import Campings from "./js/components/Campings/Campings";
+import CampingPage from "./js/components/Campings/CampingPage/CampingPage";
 
-function App(props) {
+function App() {
   return (
     <Router>
       <Switch>
         <Route exact path = "/" render = {() => <Main />}></Route>
-        <Route exact path = "/campings/page/1" render = {() => <Campings />}></Route>
+        <Route exact path = "/campings/page/:page" render = {() => <Campings />}></Route>
+        <Route path = "/campings/id/:id" render = {() => <CampingPage />}></Route>
       </Switch>
     </Router>
   )
