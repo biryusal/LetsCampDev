ebd49a351cac7ded9116f0a309b9f889cda89d6f
Graudeep <grisha.perevalov@gmail.com>
2021-02-20 23:27:35 +0500

    Добавил аттрибут для name для ссылок.

--- src/js/components/Campings/CampingCard/CampingCard.js
+++ src/js/components/Campings/CampingCard/CampingCard.js
@@ -9,22 +9,22 @@ export default (props) => {
       id = number - 1;
   return (
     <div className = "campingCard__item">
-      <NavLink exact to = {"/campings/id/"+ id}>
+      <NavLink name = "campingLink" exact to = {"/campings/id/"+ id}>
         <div className = "campingCard__imgWrapper">
-          <img className = "campingCard__img" src = {CampingPlugImage}></img>
+          <img alt = "Фотография кемпинга" className = "campingCard__img" src = {CampingPlugImage}></img>
         </div>
       </NavLink>
       <div className = "campingCard__rating">
         <div className = "campingCard__star">
           <Star />
           <span className = "campingCard__starValue">{rate ? rate : 0}</span>
-          <NavLink exact to = {"/campings/id/"+ id}>
+          <NavLink name = "campingLink" exact to = {"/campings/id/"+ id}>
             <span className = "campingCard__reviews">25 отзывов</span>
           </NavLink>
         </div>
       </div>
       <div className = "campingCard__info">
-        <NavLink exact to = {"/campings/id/"+ id}>
+        <NavLink name = "campingLink" exact to = {"/campings/id/"+ id}>
           <h3 className = "campingCard__name">{name}</h3>
         </NavLink>
         <span className = "campingCard__price">{pricePerDay} рублей/ночь</span>
