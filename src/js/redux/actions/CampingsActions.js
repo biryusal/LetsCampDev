b446777533578e8c087f55f46958923a57ed09c2
Graudeep <grisha.perevalov@gmail.com>
2021-02-21 02:48:24 +0500

    Добавил новые типы в Redux.
    Для:
    -Обновления фильтров
    -Загрузки данных с БД
    -Асинхронной загрузки фильтрованных данных с БД

--- src/js/redux/actions/CampingsActions.js
+++ src/js/redux/actions/CampingsActions.js
@@ -1,5 +1,9 @@
-import { asyncGetAmountOfCampings, asyncGetCampingById, getCampingsByPageIdAndSize } from "../../functions";
-import { GET_AMOUNT_OF_CAMPINGS_FAILURE, GET_AMOUNT_OF_CAMPINGS_REQUEST, GET_AMOUNT_OF_CAMPINGS_SUCCESS, GET_CAMPINGS_BY_PAGEID_FAILURE, GET_CAMPINGS_BY_PAGEID_REQUEST, GET_CAMPINGS_BY_PAGEID_SUCCESS, GET_CAMPING_BY_ID_FAILURE, GET_CAMPING_BY_ID_REQUEST, GET_CAMPING_BY_ID_SUCCESS } from "../types/types"
+import React from "react";
+import { Redirect, useHistory } from "react-router-dom";
+import { keys } from "regenerator-runtime";
+import { database } from "../../firebase";
+import { asyncGetAmountOfCampings, asyncGetCampingById, getCampingsByPageIdAndSize, revokeSpecialFiltersCheckboxes, getCampingByFilter } from "../../functions";
+import { APPLY_FILTERS_FAILURE, APPLY_FILTERS_REDIRECT, APPLY_FILTERS_REQUEST, APPLY_FILTERS_SUCCESS, CHANGE_FILTERS, GET_AMOUNT_OF_CAMPINGS_FAILURE, GET_AMOUNT_OF_CAMPINGS_REQUEST, GET_AMOUNT_OF_CAMPINGS_SUCCESS, GET_CAMPINGS_BY_PAGEID_FAILURE, GET_CAMPINGS_BY_PAGEID_REQUEST, GET_CAMPINGS_BY_PAGEID_SUCCESS, GET_CAMPING_BY_ID_FAILURE, GET_CAMPING_BY_ID_REQUEST, GET_CAMPING_BY_ID_SUCCESS, GET_FILTERED_CAMPINGS_BY_PAGEID_FAILURE, GET_FILTERED_CAMPINGS_BY_PAGEID_REQUEST, GET_FILTERED_CAMPINGS_BY_PAGEID_SUCCESS, RESET_FILTERS_REDIRECT, REVOKE_FILTERS, UPDATE_AMOUNT_OF_CAMPINGS } from "../types/types"
 
 export function getCampingsByPageId(pageID) {
   return async function(dispatch) {
@@ -28,6 +32,43 @@ export function getCampingsByPageId(pageID) {
     else {
       dispatch({
         type: GET_CAMPINGS_BY_PAGEID_FAILURE,
+        payload: errorMessage
+      })
+    }
+  }
+}
+
+export function getFilteredCampingsByPageId(pageID, campings, pageSize = 20) {
+  return async function(dispatch) {
+    dispatch({
+      type: GET_FILTERED_CAMPINGS_BY_PAGEID_REQUEST
+    })
+
+    let currentCampings = [],
+        errorMessage = null;
+    
+    try {
+      for (let i = (pageID-1)*pageSize + (pageID-1); i < (pageID*pageSize + (pageID-1)); i++) {
+        if (typeof campings[i] !== "undefined") {
+          currentCampings.push(campings[i]);
+        }
+      }
+    }
+
+    catch(error) {
+      errorMessage = error;
+    }
+
+    if (errorMessage === null) {
+      dispatch({
+        type: GET_FILTERED_CAMPINGS_BY_PAGEID_SUCCESS,
+        payload: currentCampings
+      })
+    }
+
+    else {
+      dispatch({
+        type: GET_FILTERED_CAMPINGS_BY_PAGEID_FAILURE,
         payload: errorMessage
       })
     }
@@ -99,3 +140,122 @@ export function getAmountOfCampings() {
     }
   }
 }
+
+export function applyFilters(filters) {
+  return async function(dispatch) {
+    let filteredCampings = [],
+        isFiltersRevoked = null,
+        errorMessage = null;
+
+    dispatch({
+      type: APPLY_FILTERS_REQUEST
+    })
+
+    try {
+      let amountOfFalseKeys = 0;
+
+      for (let key in filters) {
+        if (filters[key]) {
+          let camps = await getCampingByFilter(key);
+          if (filteredCampings.length == 0) {
+            Object.keys(camps).forEach(function (key) {
+              filteredCampings[key] = camps[key];
+            });
+          }
+          else {
+            let newArr = [];
+            Object.keys(camps).forEach(function (key) {
+              newArr[key] = camps[key];
+            });
+            for (let i = 0; i < newArr.length; i++) {
+              if (typeof newArr[i] === "undefined") {
+                delete filteredCampings[i];
+              }
+            }
+          }
+        }
+        else {
+          amountOfFalseKeys++;
+        }
+      }
+
+      if (amountOfFalseKeys == Object.keys(filters).length) {
+        isFiltersRevoked = true;
+        filteredCampings = await getCampingsByPageIdAndSize(1);
+      }
+
+      else {
+        filteredCampings = filteredCampings.filter(function (el) {
+          return el != null;
+        });
+      }
+    }
+
+    catch(e) {
+      console.log(e);
+      errorMessage = e.message;
+    }
+
+    if (errorMessage === null) {
+      dispatch({
+        type: APPLY_FILTERS_SUCCESS,
+        payload: filteredCampings
+      });
+      if (isFiltersRevoked) {
+        dispatch({
+          type: REVOKE_FILTERS
+        })
+      }
+      dispatch(applyFiltersRedirect("campings/page/1"));
+      dispatch(getFilteredCampingsByPageId(1, filteredCampings));
+    }
+
+    else {
+      dispatch({
+        type: APPLY_FILTERS_FAILURE,
+        payload: errorMessage
+      })
+    }
+  }
+}
+
+export function changeFilters(e) {
+  let value = document.getElementById(e.target.name).checked;
+  return (dispatch) => {
+    dispatch({
+      type: CHANGE_FILTERS,
+      payload: {
+        [e.target.name]: value
+      }
+    })
+  }
+}
+
+export function applyFiltersRedirect() {
+  return {
+    type: APPLY_FILTERS_REDIRECT
+  }
+}
+
+export function resetFiltersRedirect() {
+  return {
+    type: RESET_FILTERS_REDIRECT
+  }
+}
+
+export function updateAmountOfCampings(amount) {
+  return {
+    type: UPDATE_AMOUNT_OF_CAMPINGS,
+    payload: amount
+  }
+}
+
+export function revokeSpecialFilters() {
+  return (dispatch) => {
+    dispatch({
+      type: REVOKE_FILTERS
+    })
+    revokeSpecialFiltersCheckboxes();
+  }
+}
+
--- src/js/redux/reducers/CampingsReducer.js
+++ src/js/redux/reducers/CampingsReducer.js
@@ -1,16 +1,20 @@
-import { GET_AMOUNT_OF_CAMPINGS_FAILURE, GET_AMOUNT_OF_CAMPINGS_REQUEST, GET_AMOUNT_OF_CAMPINGS_SUCCESS, GET_CAMPINGS_BY_PAGEID_FAILURE, GET_CAMPINGS_BY_PAGEID_REQUEST, GET_CAMPINGS_BY_PAGEID_SUCCESS, GET_CAMPING_BY_ID_FAILURE, GET_CAMPING_BY_ID_REQUEST, GET_CAMPING_BY_ID_SUCCESS } from "../types/types";
+import { APPLY_FILTERS_FAILURE, APPLY_FILTERS_REDIRECT, APPLY_FILTERS_REQUEST, APPLY_FILTERS_SUCCESS, CHANGE_FILTERS, GET_AMOUNT_OF_CAMPINGS_FAILURE, GET_AMOUNT_OF_CAMPINGS_REQUEST, GET_AMOUNT_OF_CAMPINGS_SUCCESS, GET_CAMPINGS_BY_PAGEID_FAILURE, GET_CAMPINGS_BY_PAGEID_REQUEST, GET_CAMPINGS_BY_PAGEID_SUCCESS, GET_CAMPING_BY_ID_FAILURE, GET_CAMPING_BY_ID_REQUEST, GET_CAMPING_BY_ID_SUCCESS, GET_FILTERED_CAMPINGS_BY_PAGEID_FAILURE, GET_FILTERED_CAMPINGS_BY_PAGEID_REQUEST, GET_FILTERED_CAMPINGS_BY_PAGEID_SUCCESS, RESET_FILTERS_REDIRECT, REVOKE_FILTERS, UPDATE_AMOUNT_OF_CAMPINGS } from "../types/types";
 
 const initialState = {
-  isFetching: null,
   isUserLoggedIn: null,
   isUserInitializing: null,
   campings: [],
   currentCamping: null,
+  currentFilteredCampings: null,
   campingsError: null,
   isAmountOfCampingsFetching: null,
   amountOfCampings: null,
   amountOfCampingsError: null,
-  filterSettings: {
+  isCampingsLoading: null,
+  filtersError: null,
+  filterApplied: null,
+  filterRedirect: null,
+  campingFilters: {
     WiFi: false,
     animals: false,
     electricity: false,
@@ -19,7 +23,7 @@ const initialState = {
     kids: false,
     nonsmokeZone: false,
     parkSpace: false,
-    peopleDisabilites: false,
+    peopleDisabilities: false,
     sleepSpace: false
   }
 }
@@ -27,24 +31,59 @@ const initialState = {
 export default function CampingsReducer(state = initialState, action) {
   switch (action.type) {
     case GET_CAMPINGS_BY_PAGEID_REQUEST:
-      return {...state, isFetching: true}
+      return {...state, isCampingsLoading: true}
     case GET_CAMPINGS_BY_PAGEID_SUCCESS:
-      return {...state, isFetching: false, campings: action.payload}
+      return {...state, isCampingsLoading: false, campings: action.payload}
     case GET_CAMPINGS_BY_PAGEID_FAILURE:
-      return {...state, isFetching: false, campingsError: action.payload}
+      return {...state, isCampingsLoading: false, campingsError: action.payload}
+    case GET_FILTERED_CAMPINGS_BY_PAGEID_REQUEST:
+      return {...state, isFilterApplying: true}
+    case GET_FILTERED_CAMPINGS_BY_PAGEID_SUCCESS:
+      return {...state, isFilterApplying: false, currentFilteredCampings: action.payload}
+    case GET_FILTERED_CAMPINGS_BY_PAGEID_FAILURE:
+      return {...state, isFilterApplying: false, campingsError: action.payload}
     case GET_CAMPING_BY_ID_REQUEST:
-      return {...state, isFetching: true, campingsError: null}
+      return {...state, isCampingsLoading: true, campingsError: null}
     case GET_CAMPING_BY_ID_SUCCESS:
-      return {...state, isFetching: false, campingsError: null, currentCamping: action.payload}
+      return {...state, isCampingsLoading: false, campingsError: null, currentCamping: action.payload}
     case GET_CAMPING_BY_ID_FAILURE:
-      return {...state, isFetching: false, campingsError: action.payload}
+      return {...state, isCampingsLoading: false, campingsError: action.payload}
     case GET_AMOUNT_OF_CAMPINGS_REQUEST:
       return {...state, isAmountOfCampingsFetching: true}
     case GET_AMOUNT_OF_CAMPINGS_SUCCESS:
       return {...state, isAmountOfCampingsFetching: false, amountOfCampings: action.payload}
     case GET_AMOUNT_OF_CAMPINGS_FAILURE:
       return {...state, isAmountOfCampingsFetching: false, amountOfCampingsError: action.payload}
+    case UPDATE_AMOUNT_OF_CAMPINGS:
+      return {...state, amountOfCampings: action.payload}
+    case CHANGE_FILTERS:
+      return {...state, campingFilters: Object.assign({}, state.campingFilters, action.payload) }
+    case REVOKE_FILTERS:
+      return {...state, campingFilters: {
+        WiFi: false,
+        animals: false,
+        electricity: false,
+        food: false,
+        isWater: false,
+        kids: false,
+        nonsmokeZone: false,
+        parkSpace: false,
+        peopleDisabilities: false,
+        sleepSpace: false
+      }, filterApplied: false}
+    case APPLY_FILTERS_REDIRECT:
+      return {...state, filterRedirect: true}
+    case APPLY_FILTERS_REQUEST:
+      return {...state, isCampingsLoading: true}
+    case APPLY_FILTERS_SUCCESS:
+      return {...state, campings: action.payload, isCampingsLoading: false, filterApplied: true}
+    case APPLY_FILTERS_FAILURE:
+      return {...state, isCampingsLoading: false, filtersError: action.payload, filterApplied: false}
+    case RESET_FILTERS_REDIRECT:
+      return {...state, filterRedirect: null}
     default:
       return state;
   }
 }
+
+
