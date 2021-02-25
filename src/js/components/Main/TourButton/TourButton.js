f934913760ee4a372012996760c76cc1b23547d9
Graudeep <grisha.perevalov@gmail.com>
2021-02-26 03:57:43 +0500

    Поменял семантику TourButton на главной.

--- src/js/components/Main/TourButton/TourButton.js
+++ src/js/components/Main/TourButton/TourButton.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./TourButton.scss";
 
 export default () => {
@@ -6,7 +7,9 @@ export default () => {
     <div className = "tourButton__wrapper">
       <h2 className = "tourButton__header">LetsCamp</h2>
       <p className = "tourButton__subheader">Вслед за путешествиями</p>
-      <button className = "tourButton__button">Поиск кемпингов</button>
+      <button className = "tourButton__button">
+        <NavLink to = "campings/page/1" className = "tourButton__link">Поиск кемпингов</NavLink>
+      </button>
     </div>
   )
 }
