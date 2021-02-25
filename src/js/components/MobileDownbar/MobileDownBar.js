5e50a0dce0a3a91268f7f6ab8369fd07abd41046
Graudeep <grisha.perevalov@gmail.com>
2021-02-26 03:58:03 +0500

    Заменил иконки PNG на SVG в мобильном Downbar.

--- src/js/components/MobileDownbar/MobileDownBar.js
+++ src/js/components/MobileDownbar/MobileDownBar.js
@@ -1,10 +1,9 @@
 import React, {useState} from "react";
 import {NavLink} from "react-router-dom";
-import {connect} from "react-redux";
-import MainPage from "../../../img/homepage.png";
-import Campings from "../../../img/campings.png";
-import Reviews from "../../../img/reviews.png";
-import Auth from "../../../img/user.png";
+import HomeIcon from "../../../img/HomeIcon.svg";
+import CampingIcon from "../../../img/CampingsIcon.svg";
+import ReviewsIcon from "../../../img/star.svg";
+import AuthIcon from "../../../img/user.svg";
 import "./MobileDownbar.less";
 
 export default function MobileDownbar(props) {
@@ -13,25 +12,32 @@ export default function MobileDownbar(props) {
     <div className = "mobileDownbar__wrapper">
       <NavLink exact to = "/" activeClassName = "mobileDownbar__link_active" className = "mobileDownbar__link">
         <div className = "mobileDownbar__option">
-          <img src = {MainPage} alt = "Главная" className = "mobileDownbar__img"></img>
+          <HomeIcon />
           <span className = "mobileDownbar__description">Главная</span>
         </div>
       </NavLink>
-      <NavLink to = "/campings/page/" activeClassName = "mobileDownbar__link_active" className = "mobileDownbar__link">
+      <NavLink to = "/campings/page/1" isActive = {(match, location) => {
+        if (location.pathname.includes("/campings/page/") || location.pathname.includes("/campings/id/")) {
+          return true;
+        }
+        else {
+          return false;
+        }
+      }} activeClassName = "mobileDownbar__link_active" className = "mobileDownbar__link">
         <div className = "mobileDownbar__option">
-          <img src = {Campings} alt = "Кемпинги" className = "mobileDownbar__img"></img>
+          <CampingIcon />
           <span className = "mobileDownbar__description">Кемпинги</span>
         </div>
       </NavLink>
-      <NavLink exact to = "/" activeClassName = "mobileDownbar__link_active" className = "mobileDownbar__link">
+      <NavLink exact to = "/reviews" activeClassName = "mobileDownbar__link_active" className = "mobileDownbar__link">
         <div className = "mobileDownbar__option">
-          <img src = {Reviews} alt = "Отзывы" className = "mobileDownbar__img"></img>
+          <ReviewsIcon />
           <span className = "mobileDownbar__description">Отзывы</span>
         </div>
       </NavLink>
       <NavLink exact to = "/signin" activeClassName = "mobileDownbar__link_active" className = "mobileDownbar__link">
         <div className = "mobileDownbar__option">
-          <img src = {Auth} alt = "Вход" className = "mobileDownbar__img"></img>
+          <AuthIcon />
           <span className = "mobileDownbar__description">{isUserLoggedIn ? "Личный кабинет" : "Вход"}</span>
         </div>
       </NavLink>
