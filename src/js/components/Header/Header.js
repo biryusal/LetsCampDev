ddcba6cd455bc25c2d6a3e1cb31fe20e65f1015c
Graudeep <grisha.perevalov@gmail.com>
2021-02-28 17:18:53 +0500

    Добавил анимации и навигацию в хэдере.

--- src/js/components/Header/Header.js
+++ src/js/components/Header/Header.js
@@ -6,20 +6,49 @@ import User from "../../../img/user.png";
 import {NavLink} from "react-router-dom";
 import "./Header.scss";
 import SideModul from "../SideModul";
+import { setHeaderClosedOnShadowClick } from "../../functions";
 
 export default (props) => {
   let {isScrolled} = props;
 
+  function routerHandler() {
+    let header = document.getElementById("header"),
+        headerRouter = document.getElementById("headerRouter");
+    console.log(headerRouter);
+    if (header.classList.contains("header_opened")) {
+      header.classList.remove("header_opened");
+      header.classList.add("header_closed");
+      headerRouter.classList.remove("header__router_scrolled");
+      headerRouter.classList.add("header__router_disabled");
+    }
+
+    else {
+      header.classList.remove("header_closed");
+      header.classList.add("header_opened");
+      headerRouter.classList.remove("header__router_disabled");
+      headerRouter.classList.add("header__router_scrolled");
+
+      document.addEventListener("click", (e) => setHeaderClosedOnShadowClick(e));
+    }
+  }
+
   return(
-    <header className = {isScrolled ? "header header_scrolled header_fixed" : "header"}>
+    <header id = "header" className = {isScrolled ? "header header_scrolled header_fixed" : "header"}>
       <div className = "container header__wrapper header__wrapper_desktop">
         <NavLink to = "/">
           <img className = "header__logo" src = {isScrolled ? BlackLogo : WhiteLogo} alt = "Логотип LetsCamp"></img>
         </NavLink>
-        {isScrolled ? <input type = "text" className = "header__input" placeholder = "Куда едем?"></input> :
+        {isScrolled ? <input type = "text" onClick = {routerHandler} className = "header__input" placeholder = "Куда едем?"></input> :
         <div className = {isScrolled ? "header__links header__links_scrolled" : "header__links"}>
           <NavLink className = "header__link" activeClassName = "header__link_active" exact to = "/">Главная</NavLink>
-          <NavLink className = "header__link" activeClassName = "header__link_active" exact to = "/campings/page/1">Кемпинги</NavLink>
+          <NavLink isActive = {(match, location) => {
+          if (location.pathname.includes("/campings/page/") || location.pathname.includes("/campings/id/")) {
+            return true;
+          }
+          else {
+            return false;
+          }
+         }} className = "header__link" activeClassName = "header__link_active" exact to = "/campings/page/1">Кемпинги</NavLink>
           <NavLink className = "header__link" activeClassName = "header__link_active" exact to = "/reviews">Отзывы</NavLink>
         </div>}
         <button id = "sideModulButton" className = {isScrolled ? "header__auth header__auth_gray modalWindowButton" : "header__auth modalWindowButton"}>
@@ -28,8 +57,20 @@ export default (props) => {
         </button>
         <SideModul/>
       </div>
+      <div id = "headerRouter" className = {isScrolled ? "container header__router header__router_disabled" : "header__router_disabled"}>
+        <NavLink className = "header__link_scrolled" activeClassName = "header__link_scrolled_active" exact to = "/">Главная</NavLink>
+        <NavLink isActive = {(match, location) => {
+        if (location.pathname.includes("/campings/page/") || location.pathname.includes("/campings/id/")) {
+          return true;
+        }
+        else {
+          return false;
+        }
+      }} className = "header__link_scrolled" activeClassName = "header__link_scrolled_active" exact to = "/campings/page/1">Кемпинги</NavLink>
+        <NavLink className = "header__link_scrolled" activeClassName = "header__link_scrolled_active" exact to = "/reviews">Отзывы</NavLink>
+      </div>
       <div className = "container header__wrapper header__wrapper_mobile">
-        <input type = "text" className = "header__input" placeholder = "Куда едем?"></input>
+        <input onClick = {routerHandler} type = "text" className = "header__input" placeholder = "Куда едем?"></input>
       </div>
     </header>
   )
