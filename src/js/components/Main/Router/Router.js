e2f8ffba9c38ce4077a44602cae2e362f7739847
Graudeep <grisha.perevalov@gmail.com>
2021-03-04 02:09:43 +0500

    Вставил компонент выбора региона в роутер на главной странице.

--- src/js/components/Main/Router/Router.js
+++ src/js/components/Main/Router/Router.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
+import SelectCity from "../../SelectCity";
 import "./Router.scss";
 
-export default () => {
+function Router(props) {
+  const {region} = props;
   return (
-    <div className = "router__wrapper">
-      <div className = "router__option">
+    <div className = "router router__wrapper">
+      <div className = "router__option modalWindowButton" id = "selectCityButton">
         <div className = "router__info">
           <span className = "router__header">Местоположение</span>
-          <span className = "router__text">Куда едете?</span>
+          <span className = "router__city">{region ? region : "Куда едете?"} </span>
         </div>
       </div>
+      <SelectCity selectedRegion = {region ? region : null}/>
       <div className = "router__option">
         <div className = "router__info">
           <span className = "router__header">Прибытие</span>
@@ -31,3 +35,11 @@ export default () => {
     </div>
   )
 }
+
+const MapStateToProps = (state) => {
+  return {
+    region: state.userSelectedRegion
+  }
+}
+
+export default connect(MapStateToProps, null)(Router);
