5ac91b8b148cf5150ecb420fb1411451009da291
Graudeep <grisha.perevalov@gmail.com>
2021-03-05 23:31:01 +0500

    Подключил выбор гостей и дата пикеры в роутер на главной странице.

--- src/js/components/Main/Router/Router.js
+++ src/js/components/Main/Router/Router.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import { connect } from "react-redux";
 import SelectCity from "../../SelectCity";
+import DatePicker, {registerLocale, setDefaultLocale} from "react-datepicker";
+import {ru} from "date-fns/esm/locale";
 import "./Router.scss";
+import "react-datepicker/dist/react-datepicker.css";
+import { addDays } from "date-fns";
+import SelectGuests from "../../SelectGuests";
 
 function Router(props) {
-  const {region} = props;
+  const {region, datepickerFrom, datepickerTo} = props;
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState();
+  registerLocale("ru", ru);
+  function handleDateChange(dateName, dateValue) {
+    if (dateName === "startDateTime") {
+      setStartDate(dateValue);
+      console.log(startDate);
+      setEndDate(addDays(dateValue, 1));
+      console.log(endDate);
+    } else {
+      setEndDate(dateValue);
+    }
+  }
   return (
     <div className = "router router__wrapper">
       <div className = "router__option modalWindowButton" id = "selectCityButton">
@@ -14,23 +32,54 @@ function Router(props) {
         </div>
       </div>
       <SelectCity selectedRegion = {region ? region : null}/>
-      <div className = "router__option">
-        <div className = "router__info">
-          <span className = "router__header">Прибытие</span>
-          <span className = "router__text">Когда?</span>
-        </div>
-      </div>
-      <div className = "router__option">
-        <div className = "router__info">
-          <span className = "router__header">Выезд</span>
-          <span className = "router__text">Когда?</span>
-        </div>
-      </div>
-      <div className = "router__option">
+      <DatePicker
+      minDate = {new Date()}
+      value = {startDate}
+      id = "datapickerFrom"
+      onCalendarOpen = {() => {
+        document.getElementById("datapickerFrom").classList.add("router__option_active");
+       }}
+      onCalendarClose = {() => {
+        document.getElementById("datapickerFrom").classList.remove("router__option_active");
+      }}
+      wrapperClassName = "datepicker__wrapper"
+      customInput = {
+          <div className = "router__datePicker">
+            <div className = "router__info">
+              <span className = "router__header">Прибытие</span>
+              <span className = "router__text">Когда?</span>
+            </div>
+          </div>}
+      locale = "ru" 
+      selected = {startDate} 
+      onChange = {date => handleDateChange("startDateTime", date)} />
+      <DatePicker
+      value = {endDate}
+      id = "datapickerTo"
+      onCalendarOpen = {() => {
+       document.getElementById("datapickerTo").classList.add("router__option_active");
+      }}
+      onCalendarClose = {() => {
+        document.getElementById("datapickerTo").classList.remove("router__option_active");
+       }}
+      wrapperClassName = "datepicker__wrapper"
+      customInput = {
+        <div className = "router__datePicker">
+          <div className = "router__info">
+            <span className = "router__header">Отбытие</span>
+            <span className = "router__text">Когда?</span>
+          </div>
+        </div>}
+      locale = "ru" 
+      selected = {endDate}
+      minDate = {startDate}
+      onChange = {date => handleDateChange("endDateTime", date)} />
+      <div className = "router__option modalWindowButton" id = "selectGuestsButton">
         <div className = "router__info">
           <span className = "router__header">Количество гостей</span>
           <span className = "router__text">Сколько гостей?</span>
         </div>
+        <SelectGuests />
       </div>
     </div>
   )
@@ -38,7 +87,9 @@ function Router(props) {
 
 const MapStateToProps = (state) => {
   return {
-    region: state.userSelectedRegion
+    region: state.userSelectedRegion,
+    datepickerFrom: state.datepicker.from,
+    datepickerTo: state.datepicker.to
   }
 }
 
