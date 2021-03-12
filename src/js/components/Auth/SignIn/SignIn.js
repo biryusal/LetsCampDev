e42195854f6a50d9334fe160384c1870c8a19d0c
Graudeep <grisha.perevalov@gmail.com>
2021-03-12 21:02:02 +0300

    Добавил файл для автризации.

--- src/js/components/Auth/SignIn/SignIn.js
+++ src/js/components/Auth/SignIn/SignIn.js
@@ -3,8 +3,10 @@ import "./SignIn.scss";
 
 export default () => {
   return (
-    <div>
-      123
+    <div className = "container signin__wrapper">
+      <h2 className = "signin__header">
+        This is SignIn page - you are welcome.
+      </h2>
     </div>
   )
-}
+};
