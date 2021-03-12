b51c296753a7b926eab689d561c97902081ce1ac
Graudeep <grisha.perevalov@gmail.com>
2021-03-12 21:02:14 +0300

    Добавил файл для регистрации.

---
+++ src/js/components/Auth/SignUp/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SignUp.scss";
+
+export default () => {
+  return (
+    <div className = "container signup__wrapper">
+      <h2 className = "signup__header">
+        This is SignUp page - you are welcome.
+      </h2>
+    </div>
+  )
+};
