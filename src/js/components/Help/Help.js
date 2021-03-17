fc0cb745e1691d646aab9676fc77c5851a3d62f7
Graudeep <grisha.perevalov@gmail.com>
2021-03-18 02:48:40 +0300

    Добавил заглушку для ссылки "Помощь" из SideModul.

---
+++ src/js/components/Help/Help.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Help.scss";
+
+export default () => {
+  return (
+    <div className = "container help__wrapper">
+      <h2 className = "help__header">
+        This is Help page - you are welcome.
+      </h2>
+    </div>
+  )
+};
